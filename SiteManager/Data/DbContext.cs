7271471cbe837b675b9b68a99384ffb1fae19f48
prager <jkulisek.us@gmail.com>
2025-07-20 15:32:29 -0700

    Initial commit

---
+++ SiteManager/Data/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppAuthSQLite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+
+    public class Note
+    {
+        public int Id { get; set; }  // ✅ This is treated as the primary key by convention
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
