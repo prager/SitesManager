3d9cb730ae6561a8df64e248a3aa73ebac52e95b
prager <jkulisek.us@gmail.com>
2025-07-20 16:47:50 -0700

    Added HTML and Bootstrap wireframe

--- SiteManager/obj/Debug/net8.0/SiteManager.AssemblyInfo.cs
+++ SiteManager/obj/Debug/net8.0/SiteManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7271471cbe837b675b9b68a99384ffb1fae19f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
